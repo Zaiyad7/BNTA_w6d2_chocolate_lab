06199f341878bc07b338109292525c08f1ca3235
Zaiyad7 <ZaiyadZakaria@live.co.uk>
2023-08-08 16:41:39 +0100

    finished annotation

--- chocolate_start_point/src/main/java/com/bnta/chocolate/models/Chocolate.java
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/models/Chocolate.java
@@ -1,19 +1,24 @@
 package com.bnta.chocolate.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 
 public class Chocolate {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @Column(name = "name")
     private String name;
-
+    @Column(name = "cocoa_percentage")
     private int cocoaPercentage;
 
-
+    @ManyToOne
+    @JoinColumn(name = "estate_id")
+//    @JsonIgnoreProperties
     private Estate estate;
 
     public Chocolate(String name, int cocoaPercentage, Estate estate) {
